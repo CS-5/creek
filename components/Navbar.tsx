8f38885425b8aa83b6b98f62ab0132d920b5ad0a
CS-5 <me@carsonseese.com>
2022-03-16 11:23:09 -0400

    Working authentication

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,9 @@
-import { useAuth0 } from "@auth0/auth0-react";
 import Link from "next/link";
 import { FunctionComponent } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Navbar: FunctionComponent = () => {
-  const { loginWithRedirect } = useAuth0();
+  const { isAuthenticated, logout, loginWithRedirect } = useAuth0();
 
   return (
     <div className="navbar mb-2 shadow-lg bg-neutral text-neutral-content rounded-box">
@@ -13,7 +13,7 @@ const Navbar: FunctionComponent = () => {
       <div className="flex-none hidden px-2 mx-2 lg:flex">
         <div className="flex items-stretch">
           <Link href="/live">
-            <button className="btn btn-ghost btn-sm rounded-btn">
+            <div className="btn btn-ghost btn-sm rounded-btn">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-6 w-6 mr-2"
@@ -29,10 +29,10 @@ const Navbar: FunctionComponent = () => {
                 />
               </svg>
               Live
-            </button>
+            </div>
           </Link>
           <Link href="/videos">
-            <button className="btn btn-ghost btn-sm rounded-btn">
+            <div className="btn btn-ghost btn-sm rounded-btn">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-6 w-6 mr-2"
@@ -47,14 +47,40 @@ const Navbar: FunctionComponent = () => {
                   d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z"
                 />
               </svg>
-              Video Archive
-            </button>
+              Videos
+            </div>
           </Link>
+          {isAuthenticated && (
+            <Link href="/settings">
+              <a className="btn btn-ghost btn-sm rounded-btn">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 mr-2"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
+                  />
+                </svg>
+                Settings
+              </a>
+            </Link>
+          )}
+
           <button
-            onClick={loginWithRedirect}
-            className="btn btn-ghost btn-sm rounded-btn"
+            className="btn btn-outline btn-primary btn-sm rounded-btn"
+            onClick={
+              isAuthenticated
+                ? () => logout({ returnTo: window.location.origin })
+                : loginWithRedirect
+            }
           >
-            Login
+            {isAuthenticated ? "Logout" : "Login"}
           </button>
         </div>
       </div>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,28 @@
 import "../styles/globals.css";
 import { AppProps } from "next/app";
 import Head from "next/head";
-import AuthenticatedNavbar from "../components/navigation/Authenticated";
-import PublicNavbar from "../components/navigation/Public";
+import Navbar from "../components/Navbar";
 import { ReactNode } from "react";
 import { Auth0Provider, useAuth0 } from "@auth0/auth0-react";
 
 export default function App({ Component, pageProps }: AppProps): ReactNode {
-  const { isAuthenticated } = useAuth0();
+  const callbackDomain =
+    typeof window !== "undefined"
+      ? `${window.location.protocol}//${window.location.host}`
+      : "http://localhost:3000";
 
   return (
     <Auth0Provider
-      domain={process.env.AUTH0_DOMAIN ?? ""}
-      clientId={process.env.AUTH0_CLIENT_ID ?? ""}
-      redirectUri={window.location.origin}
+      domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN ?? ""}
+      clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID ?? ""}
+      redirectUri={callbackDomain}
     >
       <div data-theme="forest">
         <Head>
           <title>Creek</title>
           <meta property="og:title" content="Creek" key="title" />
         </Head>
-        {isAuthenticated ? <AuthenticatedNavbar /> : <PublicNavbar />}
+        <Navbar />
         <Component {...pageProps} />
       </div>
     </Auth0Provider>
