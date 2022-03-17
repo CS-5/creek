3747ba93eb3f21c4c8313ebedb8840cea63e5695
Carson Seese <me@carsonseese.com>
2022-03-17 17:25:59 +0000

    Initial API code

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -1,9 +1,14 @@
 import type { NextPage } from "next";
 import { useAuth0 } from "@auth0/auth0-react";
 import Error from "../components/error";
+import { useEffect, useState } from "react";
 
 const Settings: NextPage = () => {
+  const [data, setData] = useState<any>(null);
+  const [loading, setLoading] = useState(false);
   const { isLoading, isAuthenticated } = useAuth0();
+
+  setLoading(isLoading);
 
   if (!isAuthenticated && !isLoading) {
     return (
@@ -14,9 +19,23 @@ const Settings: NextPage = () => {
     );
   }
 
+  useEffect(() => {
+    setLoading(true);
+
+    fetch("api/default/info")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      });
+  });
+
+  if (loading) return <p>Loading...</p>;
+
   return (
     <div className="prose">
       <h1>Settings</h1>
+      <pre>{data}</pre>
     </div>
   );
 };
