8d4ad451ef916526cc9ce9db17954d8e15042440
CS-5 <me@carsonseese.com>
2022-04-10 09:21:32 -0400

    Fix login button handling

--- components/layout/landingHeader.tsx
+++ components/layout/landingHeader.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { FunctionComponent } from "react";
 import Logo from "../logo";
 import { useAuth0 } from "@auth0/auth0-react";
+import { useRouter } from "next/router";
 
 interface Props {
   className?: string;
@@ -9,6 +10,7 @@ interface Props {
 
 export const LandingHeader: FunctionComponent<Props> = (props: Props) => {
   const { loginWithRedirect, isAuthenticated } = useAuth0();
+  const router = useRouter();
 
   return (
     <header {...props}>
@@ -18,28 +20,28 @@ export const LandingHeader: FunctionComponent<Props> = (props: Props) => {
         </div>
         <div className="flex-none hidden py-auto px-2 mx-2 lg:flex">
           <div className="flex items-center">
-            <Link href="/app" passHref>
-              <button
-                className="btn btn-ghost btn-sm rounded-btn"
-                onClick={isAuthenticated ? undefined : loginWithRedirect}
+            <button
+              className="btn btn-ghost btn-sm rounded-btn"
+              onClick={
+                isAuthenticated ? () => router.push("/app") : loginWithRedirect
+              }
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 mr-2"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6 mr-2"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M8 14v3m4-3v3m4-3v3M3 21h18M3 10h18M3 7l9-4 9 4M4 10h16v11H4V10z"
-                  />
-                </svg>
-                {isAuthenticated ? "Admin" : "Login"}
-              </button>
-            </Link>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M8 14v3m4-3v3m4-3v3M3 21h18M3 10h18M3 7l9-4 9 4M4 10h16v11H4V10z"
+                />
+              </svg>
+              {isAuthenticated ? "Admin" : "Login"}
+            </button>
           </div>
         </div>
       </div>
