738e557531941cc224ca4082a095643f3b39f478
CS-5 <me@carsonseese.com>
2022-04-11 13:30:31 -0400

    Add QueryClientProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,14 +6,17 @@ import { Layout as HomeLayout } from "../components/layout/home";
 import { ReactNode } from "react";
 import { Auth0Provider } from "@auth0/auth0-react";
 import { useRouter } from "next/router";
+import { QueryClientProvider, QueryClient } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 export default function App({ Component, pageProps }: AppProps): ReactNode {
-  const router = useRouter();
   const callbackDomain =
     typeof window !== "undefined"
       ? `${window.location.protocol}//${window.location.host}`
       : "http://localhost:3000";
 
+  const router = useRouter();
+  const queryClient = new QueryClient();
   const Layout = router.pathname.startsWith("/app") ? AppLayout : HomeLayout;
 
   return (
@@ -24,15 +27,18 @@ export default function App({ Component, pageProps }: AppProps): ReactNode {
       audience={process.env.NEXT_PUBLIC_AUTH0_FRONTEND_AUDIENCE ?? ""}
       scope={process.env.NEXT_PUBLIC_AUTH0_FRONTEND_SCOPE ?? ""}
     >
-      <div data-theme="night">
-        <Head>
-          <title>Creek</title>
-          <meta property="og:title" content="Creek" key="title" />
-        </Head>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </div>
+      <QueryClientProvider client={queryClient}>
+        <div data-theme="night">
+          <Head>
+            <title>Creek</title>
+            <meta property="og:title" content="Creek" key="title" />
+          </Head>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+          <ReactQueryDevtools initialIsOpen={false} />
+        </div>
+      </QueryClientProvider>
     </Auth0Provider>
   );
 }
