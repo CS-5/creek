803ee1d0716a6c7878e72ff131e466b2ecd01ebc
CS-5 <me@carsonseese.com>
2022-04-28 17:55:18 -0400

    Remove query-client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,6 @@ import { AppProps } from "next/app";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { ReactNode } from "react";
-import { QueryClient, QueryClientProvider } from "react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
 import { Layout as AppLayout } from "../components/layout/app";
 import { Layout as HomeLayout } from "../components/layout/home";
 import "../styles/globals.css";
@@ -19,8 +17,6 @@ export default function App({ Component, pageProps }: AppProps): ReactNode {
       ? `${window.location.protocol}//${window.location.host}`
       : "http://localhost:3000";
 
-  const queryClient = new QueryClient();
-
   return (
     <Auth0Provider
       domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN ?? ""}
@@ -29,18 +25,19 @@ export default function App({ Component, pageProps }: AppProps): ReactNode {
       audience={process.env.NEXT_PUBLIC_AUTH0_FRONTEND_AUDIENCE ?? ""}
       scope={process.env.NEXT_PUBLIC_AUTH0_FRONTEND_SCOPE ?? ""}
     >
-      <QueryClientProvider client={queryClient}>
-        <div data-theme="night">
-          <Head>
-            <title>Creek</title>
-            <meta property="og:title" content="Creek" key="title" />
-          </Head>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-          <ReactQueryDevtools initialIsOpen={false} />
-        </div>
-      </QueryClientProvider>
+      <div data-theme="night">
+        <Head>
+          <title>Creek</title>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+          <meta property="og:title" content="Creek" key="title" />
+        </Head>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </div>
     </Auth0Provider>
   );
 }
