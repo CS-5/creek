df1c0fff10dd0460d5fa8e1402eba481ca684087
CS-5 <me@carsonseese.com>
2022-04-29 15:25:59 -0400

    Make development scripts more reusable

--- dev.sh
+++ dev.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-wrangler pages dev \
+static_assets=""
+script="-- $1"
+
+if [ -z "$1" ]; then
+  static_assets="public"
+  script=""
+fi
+
+echo $script
+
+wrangler pages dev $static_assets \
 --binding AUTH0_FRONTEND_AUDIENCE=$WORKER_AUTH0_FRONTEND_AUDIENCE \
 --binding AUTH0_DOMAIN=$WORKER_AUTH0_DOMAIN \
 --binding AUTH0_BACKEND_SECRET=$WORKER_AUTH0_BACKEND_SECRET \
 --binding AUTH0_BACKEND_ID=$WORKER_AUTH0_BACKEND_ID \
 --binding CF_ACCOUNT_ID=$WORKER_CF_ACCOUNT_ID \
 --binding CF_API_TOKEN=$WORKER_CF_API_TOKEN \
--- npm run next:dev
+$script
